a1d023d33f769bab9a11caba5c57d37a6a3abbb7
Manuel Schumacher <manuel.schumacher@pebe.ch>
2023-01-04 19:39:52 +0100

    📝: Added meta tags

--- frontend/nuxt-app/nuxt.config.ts
+++ frontend/nuxt-app/nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: "FLIMTIX.DEV",
+			meta: [
+				{ name: "author", content: "Manuel Schumacher" },
+				{ name: "description", content: "My amazing portfolio!" },
+			],
+		},
+	},
 	modules: ["@nuxtjs/tailwindcss"],
 	typescript: {
 		strict: true,
